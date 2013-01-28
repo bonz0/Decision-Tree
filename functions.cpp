60b707989bd8496b79b7900b1cf2865d330c8a12
Farhang Zia <farhang.bonzo@gmail.com>
2013-01-28 02:28:06 -0500

    Updated

--- functions.cpp
+++ functions.cpp
@@ -29,10 +29,8 @@ void parse(string& someString, vvs &attributeTable)
 void printAttributeTable(vvs &attributeTable)
 {
 	int inner, outer;
-	for (outer = 0; outer < attributeTable.size(); outer++)
-	{
-		for (inner = 0; inner < attributeTable[outer].size(); inner++)
-		{
+	for (outer = 0; outer < attributeTable.size(); outer++) {
+		for (inner = 0; inner < attributeTable[outer].size(); inner++) {
 			cout << attributeTable[outer][inner] << "\t";
 		}
 		cout << endl;
@@ -50,31 +48,23 @@ vvs pruneTable(vvs &attributeTable, string &colName, string value)
 	vvs prunedTable;
 	int column = -1;
 	vs headerRow;
-	for (iii = 0; iii < attributeTable[0].size(); iii++)
-	{
-		if (attributeTable[0][iii] == colName)
-		{
+	for (iii = 0; iii < attributeTable[0].size(); iii++) {
+		if (attributeTable[0][iii] == colName) {
 			column = iii;
 			break;
 		}
 	}
-	for (iii = 0; iii < attributeTable[0].size(); iii++)
-	{
-		 if (iii != column)
-		 {
+	for (iii = 0; iii < attributeTable[0].size(); iii++) {
+		 if (iii != column) {
 		 	headerRow.push_back(attributeTable[0][iii]);
 		 }
 	}
 	prunedTable.push_back(headerRow);
-	for (iii = 0; iii < attributeTable.size(); iii++)
-	{
+	for (iii = 0; iii < attributeTable.size(); iii++) {
 		vs auxRow;
-		if (attributeTable[iii][column] == value)
-		{
-			for (jjj = 0; jjj < attributeTable[iii].size(); jjj++)
-			{
-				if(jjj != column)
-				{
+		if (attributeTable[iii][column] == value) {
+			for (jjj = 0; jjj < attributeTable[iii].size(); jjj++) {
+				if(jjj != column) {
 					auxRow.push_back(attributeTable[iii][jjj]);
 				}
 			}
@@ -90,24 +80,19 @@ vvs pruneTable(vvs &attributeTable, string &colName, string value)
  */
 node* buildDecisionTree(vvs &table, node* nodePtr, vvs &tableInfo)
 {
-	if (tableIsEmpty(table))
-	{
+	if (tableIsEmpty(table)) {
 		return NULL;
 	}
-	if (isHomogeneous(table))
-	{
+	if (isHomogeneous(table)) {
 		nodePtr->isLeaf = true;
 		nodePtr->label = table[1][table[1].size()-1];
 		return nodePtr;
-	}
-	else
-	{
+	} else {
 		string splittingCol = decideSplittingColumn(table);
 		nodePtr->splitOn = splittingCol;
 		int colIndex = returnColumnIndex(splittingCol, tableInfo);
 		int iii;
-		for (iii = 1; iii < tableInfo[colIndex].size(); iii++)
-		{
+		for (iii = 1; iii < tableInfo[colIndex].size(); iii++) {
 			node* newNode = (node*) new node;
 			newNode->label = tableInfo[colIndex][iii];
 			nodePtr->childrenValues.push_back(tableInfo[colIndex][iii]);
@@ -131,10 +116,8 @@ bool isHomogeneous(vvs &table)
 	int iii;
 	int lastCol = table[0].size() - 1;
 	string firstValue = table[1][lastCol];
-	for (iii = 1; iii < table.size(); iii++)
-	{
-		if (firstValue != table[iii][lastCol])
-		{
+	for (iii = 1; iii < table.size(); iii++) {
+		if (firstValue != table[iii][lastCol]) {
 			return false;
 		}
 	}
@@ -151,32 +134,25 @@ vi countDistinct(vvs &table, int column)
 	vi counts;
 	bool found = false;
 	int foundIndex;
-	for (int iii = 1; iii < table.size(); iii++)
-	{
-		for (int jjj = 0; jjj < vectorOfStrings.size(); jjj++)
-		{
-			if (vectorOfStrings[jjj] == table[iii][column])
-			{
+	for (int iii = 1; iii < table.size(); iii++) {
+		for (int jjj = 0; jjj < vectorOfStrings.size(); jjj++) {
+			if (vectorOfStrings[jjj] == table[iii][column]) {
 				found = true;
 				foundIndex = jjj;
 				break;
-			}
-			else
+			} else {
 				found = false;
+			}
 		}
-		if (!found)
-		{
+		if (!found) {
 			counts.push_back(1);
 			vectorOfStrings.push_back(table[iii][column]);
-		}
-		else
-		{
+		} else {
 			counts[foundIndex]++;
 		}
 	}
 	int sum = 0;
-	for (int iii = 0; iii < counts.size(); iii++)
-	{
+	for (int iii = 0; iii < counts.size(); iii++) {
 		sum += counts[iii];
 	}
 	counts.push_back(sum);
@@ -194,28 +170,22 @@ string decideSplittingColumn(vvs &table)
 	double minEntropy = DBL_MAX;
 	int splittingColumn = 0;
 	vi entropies;
-	for (column = 0; column < table[0].size() - 1; column++)
-	{
+	for (column = 0; column < table[0].size() - 1; column++) {
 		string colName = table[0][column];
 		msi tempMap;
 		vi counts = countDistinct(table, column);
 		vd attributeEntropy;
 		double columnEntropy = 0.0;
-		for (iii = 1; iii < table.size()-1; iii++)
-		{
+		for (iii = 1; iii < table.size()-1; iii++) {
 			double entropy = 0.0;
-			if (tempMap.find(table[iii][column]) != tempMap.end()) 	// IF ATTRIBUTE IS ALREADY FOUND IN A COLUMN, UPDATE IT'S FREQUENCY
-			{
+			if (tempMap.find(table[iii][column]) != tempMap.end()) { 	// IF ATTRIBUTE IS ALREADY FOUND IN A COLUMN, UPDATE IT'S FREQUENCY
 				tempMap[table[iii][column]]++;
-			}
-			else							// IF ATTRIBUTE IS FOUND FOR THE FIRST TIME IN A COLUMN, THEN PROCESS IT AND CALCULATE IT'S ENTROPY
-			{
+			} else { 							// IF ATTRIBUTE IS FOUND FOR THE FIRST TIME IN A COLUMN, THEN PROCESS IT AND CALCULATE IT'S ENTROPY
 				tempMap[table[iii][column]] = 1;
 				vvs tempTable = pruneTable(table, colName, table[iii][column]);
 				vi classCounts = countDistinct(tempTable, tempTable[0].size()-1);
 				int jjj, kkk;
-				for (jjj = 0; jjj < classCounts.size(); jjj++)
-				{
+				for (jjj = 0; jjj < classCounts.size(); jjj++) {
 					double temp = (double) classCounts[jjj];
 					entropy -= (temp/classCounts[classCounts.size()-1])*(log(temp/classCounts[classCounts.size()-1]) / log(2));
 				}
@@ -223,13 +193,11 @@ string decideSplittingColumn(vvs &table)
 				entropy = 0.0;
 			}
 		}
-		for (iii = 0; iii < counts.size() - 1; iii++)
-		{
+		for (iii = 0; iii < counts.size() - 1; iii++) {
 			columnEntropy += ((double) counts[iii] * (double) attributeEntropy[iii]);
 		}
 		columnEntropy = columnEntropy / ((double) counts[counts.size() - 1]);
-		if (columnEntropy <= minEntropy)
-		{
+		if (columnEntropy <= minEntropy) {
 			minEntropy = columnEntropy;
 			splittingColumn = column;
 		}
@@ -244,10 +212,8 @@ string decideSplittingColumn(vvs &table)
 int returnColumnIndex(string &columnName, vvs &tableInfo)
 {
 	int iii;
-	for (iii = 0; iii < tableInfo.size(); iii++)
-	{
-		if (tableInfo[iii][0] == columnName)
-		{
+	for (iii = 0; iii < tableInfo.size(); iii++) {
+		if (tableInfo[iii][0] == columnName) {
 			return iii;
 		}
 	}
@@ -269,24 +235,19 @@ bool tableIsEmpty(vvs &table)
  */
 void printDecisionTree(node* nodePtr)
 {
-	if(nodePtr == NULL)
-	{
+	if(nodePtr == NULL) {
 		return;
 	}
-	if (!nodePtr->children.empty())
-        {
+	if (!nodePtr->children.empty()) {
 		cout << " Value: " << nodePtr->label << endl;
 		cout << "Split on: " << nodePtr->splitOn;
 		int iii;
-		for (iii = 0; iii < nodePtr->children.size(); iii++)
-		{   
+		for (iii = 0; iii < nodePtr->children.size(); iii++) {   
 			cout << "\t";
 			printDecisionTree(nodePtr->children[iii]);
 		}
 		return;
-        }
-	else
-	{
+        } else {
 		cout << "Predicted class = " << nodePtr->label;
 		return;
 	}
@@ -302,14 +263,12 @@ void printDecisionTree(node* nodePtr)
 string testDataOnDecisionTree(vs &singleLine, node* nodePtr, vvs &tableInfo, string defaultClass)
 {
 	string prediction;
-	while (nodePtr->isLeaf != true && !nodePtr->children.empty())
-	{
+	while (nodePtr->isLeaf != true && !nodePtr->children.empty()) {
 		int index = returnColumnIndex(nodePtr->splitOn, tableInfo);
 		string value = singleLine[index];
 		int childIndex = returnIndexOfVector(nodePtr->childrenValues, value);
 		nodePtr = nodePtr->children[childIndex];
-		if (nodePtr == NULL)
-		{
+		if (nodePtr == NULL) {
 			prediction = defaultClass;
 			break;
 		}
@@ -325,10 +284,8 @@ string testDataOnDecisionTree(vs &singleLine, node* nodePtr, vvs &tableInfo, str
 int returnIndexOfVector(vs &stringVector, string value)
 {
 	int iii;
-	for (iii = 0; iii < stringVector.size(); iii++)
-	{
-		if (stringVector[iii] == value)
-		{
+	for (iii = 0; iii < stringVector.size(); iii++) {
+		if (stringVector[iii] == value)	{
 			return iii;
 		}
 	}
@@ -346,16 +303,12 @@ double printPredictionsAndCalculateAccuracy(vs &givenData, vs &predictions)
 	int correct = 0;
 	outputFile << setw(3) << "#" << setw(16) << "Given Class" << setw(31) << right << "Predicted Class" << endl;
 	outputFile << "--------------------------------------------------" << endl;
-	for (int iii = 0; iii < givenData.size(); iii++)
-	{
+	for (int iii = 0; iii < givenData.size(); iii++) {
 		outputFile << setw(3) << iii+1 << setw(16) << givenData[iii];
-		if (givenData[iii] == predictions[iii])
-		{
+		if (givenData[iii] == predictions[iii]) {
 			correct++;
 			outputFile << "  ------------  ";
-		}
-		else
-		{
+		} else {
 			outputFile << "  xxxxxxxxxxxx  ";
 		}
 		outputFile << predictions[iii] << endl;
@@ -376,19 +329,14 @@ double printPredictionsAndCalculateAccuracy(vs &givenData, vs &predictions)
 vvs generateTableInfo(vvs &dataTable)
 {
 	vvs tableInfo;
-	for (int iii = 0; iii < dataTable[0].size(); iii++)
-	{
+	for (int iii = 0; iii < dataTable[0].size(); iii++) {
 		vs tempInfo;
 		msi tempMap;
-		for (int jjj = 0; jjj < dataTable.size(); jjj++)
-		{
-			if (tempMap.count(dataTable[jjj][iii]) == 0)
-			{
+		for (int jjj = 0; jjj < dataTable.size(); jjj++) {
+			if (tempMap.count(dataTable[jjj][iii]) == 0) {
 				tempMap[dataTable[jjj][iii]] = 1;
 				tempInfo.push_back(dataTable[jjj][iii]);
-			}
-			else
-			{
+			} else	{
 				tempMap[dataTable[jjj][iii]]++;
 			}
 		}
@@ -404,24 +352,18 @@ vvs generateTableInfo(vvs &dataTable)
 string returnMostFrequentClass(vvs &dataTable)
 {
 	msi trainingClasses;           													 // Stores the classlabels and their frequency
-	for (int iii = 1; iii < dataTable.size(); iii++)
-	{
-		if (trainingClasses.count(dataTable[iii][dataTable[0].size()-1]) == 0)
-		{
+	for (int iii = 1; iii < dataTable.size(); iii++) {
+		if (trainingClasses.count(dataTable[iii][dataTable[0].size()-1]) == 0) {
 			trainingClasses[dataTable[iii][dataTable[0].size()-1]] = 1;
-		}
-		else
-		{
+		} else {
 			trainingClasses[dataTable[iii][dataTable[0].size()-1]]++;
 		}
 	}   
 	msi::iterator mapIter;
 	int highestClassCount = 0;
 	string mostFrequentClass;
-	for (mapIter = trainingClasses.begin(); mapIter != trainingClasses.end(); mapIter++)
-	{
-		if (mapIter->second >= highestClassCount)
-		{
+	for (mapIter = trainingClasses.begin(); mapIter != trainingClasses.end(); mapIter++) {
+		if (mapIter->second >= highestClassCount) {
 			highestClassCount = mapIter->second;
 			mostFrequentClass = mapIter->first;
 		}   
